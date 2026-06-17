0c1e880f205b11bff345a2689580db946b4426d7
felispider <leidykatherinerojas393@gmail.com>
2026-06-16 19:13:22 -0500

    Update main.js

--- taller java/main.js
+++ taller java/main.js
@@ -3,38 +3,36 @@ const persona = {
     edad: 23,
     altura: 1.68,
     peso: "60 kg",
-    
+
     caminar() {
         console.log("Estoy caminando");
     }
 };
+
+console.log(persona);
+
 persona.caminar();
 
 const personaJSON = JSON.stringify(persona);
 
-
 console.log(personaJSON);
 
-colección de empleados usando objetos dentro de un arreglo
 const listaEmpleados = [
     { nombre: "Juan Pérez", apellido: "López" },
     { nombre: "Ana", apellido: "González" },
     { nombre: "Pedro", apellido: "Sánchez" }
 ];
+
 console.log(listaEmpleados);
 
 const frutas = ["Manzana", "Banana", "Naranja"];
 
-
 const posicionManzana = frutas.indexOf("Manzana");
 
-
 console.log(posicionManzana);
-
 
 const edades = [25, 30, 18, 15];
 
 const primerMenor = edades.find(edad => edad < 20);
-
 
 console.log(primerMenor);
